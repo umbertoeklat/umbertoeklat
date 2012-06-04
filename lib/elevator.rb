2fe23f9ae1cbf177fd5f8d3a54a840bdfb8c58c1
Umberto Eklat <umbertoeklat@yahoo.com>
2012-06-04 10:26:38 +0800

    initial commit

---
+++ lib/elevator.rb
@@ -0,0 +1,12 @@
+#elevator.rb
+
+class Elevator
+  attr_accessor :floors, :max_load, :allowed_floors,
+                :cur_flr, :enabled	, :door
+  
+  def initialize
+    enabled = true
+  end
+
+  
+end
---
+++ spec/elevator_spec.rb
@@ -0,0 +1,34 @@
+# elevator_spec.rb
+require 'elevator'
+#require 'building'
+
+describe Elevator, "#floors" do
+  before(:each) do
+    @elevator = Elevator.new
+	#@elevator.enabled = true
+  end
+  
+  #after(:
+  
+  it "returns number of floors" do
+	@elevator.floors = 1
+	@elevator.floors.should_not eq(0)	
+  end
+  
+  it "returns if enabled" do
+    #@elevator.enabled = true
+	puts @elevator.enabled
+    @elevator.enabled.should be_true
+  end
+  
+  subject { @elevator.enabled }
+  it {should be_true} 
+end
+
+#describe Building, "#levels" do
+#  it "should have " do
+#    bldg = Bldg.new
+#	bldg.should be_an_instance_of Bldg
+#  end
+
+#end
