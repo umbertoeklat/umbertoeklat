456773a6f7c6cb485b876953c7b56dd6da6d3190
root <root@bsc.unionbankph.com>
2012-06-06 20:52:35 +0800

    MAJOR Edits

--- lib/elevator.rb
+++ lib/elevator.rb
@@ -4,8 +4,13 @@ class Elevator
   attr_accessor :floors, :max_load, :allowed_floors,
                 :cur_flr, :enabled	, :door
   
-  def initialize (flrs, capacity, allowed_flrs, current, enabled, door_stat)
-    enabled = true
+  def initialize (flrs, capacity, allowed_flrs, current, running, door_stat)
+    self.floors = flrs
+    self.max_load = capacity
+	self.allowed_floors = allowed_flrs
+	self.cur_flr = current
+	self.enabled = running
+	self.door = door_stat
   end
 
   
--- spec/elevator_spec.rb
+++ spec/elevator_spec.rb
@@ -2,7 +2,7 @@
 require 'elevator'
 #require 'building'
 
-describe Elevator, "#floors" do
+describe Elevator, "#attributes" do
   before(:each) do
     @elevator = Elevator.new(10, 10, 0..9, 1, true, 'open')
 	#@elevator.enabled = true
@@ -10,21 +10,36 @@ describe Elevator, "#floors" do
   
   #after(:each) do
   
+  it { @elevator.should be_an_instance_of(Elevator) }
   it "returns number of floors" do
 	@elevator.floors = 1
 	@elevator.floors.should_not eq(0)	
-  end
+  end  
   
   it "returns if ENABLED " do
     #@elevator.enabled = true
-	puts @elevator.enabled
-    @elevator.enabled.should be_true
+	puts @elevator.enabled #sam
+    @elevator.enabled.should be  #nil or bool
+  end
+  
+  it "returns allowed floors :allowed_floors" do
+    @elevator.allowed_floors.should respond_to(:hash )
+	@elevator.allowed_floors.should be_a_kind_of(Range)
+  end
+  
+  it "returns current floor" do
+    @elevator.cur_flr.should be_a(Fixnum)
+	@elevator.cur_flr.should be_an(Integer)
+  end
+  
+  it "returns door status" do
+    @elevator.door.should_not be_empty
   end
   
   subject { @elevator.enabled }
   it {should be_true} 
   
-  it "should have max load capacity}"
+  it "should have max load capacity"
 end
 
 #describe Building, "#levels" do
